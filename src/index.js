a0f3493dd71b4862e6e77b51fdcf26dfeae3fb0b
Mati Joseph <crucialniccur@gmail.com>
2025-03-22 08:41:28 +0300

    write a fetch to fetch data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Your code here
+fetch("http://localhost:3000/characters")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((hitlafu) => {
+    console.error(hitlafu);
+  });
