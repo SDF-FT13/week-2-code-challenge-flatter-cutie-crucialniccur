ed87ffed4fc09c67a84f7a79e01bcf7e2badc95d
Mati Joseph <crucialniccur@gmail.com>
2025-03-22 09:16:04 +0300

    create span elements for each item in the data and show their names in the character-bar div in span elements

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ let bar = document.querySelector("#character-bar");
 fetch("http://localhost:3000/characters")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    data.forEach((item) => {
+      //create span element for each item
+      let span = document.createElement("span");
+      // set names from the fetch as textcontent
+      span.textContent = item.name;
+      //append span to bar as child
+      bar.appendChild(span);
+    });
   })
   .catch((hitlafu) => {
     console.error(hitlafu);
