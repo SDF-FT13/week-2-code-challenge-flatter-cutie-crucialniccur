c4341797af4e49479daeeba9ae6e1988521a6098
Mati Joseph <crucialniccur@gmail.com>
2025-03-25 18:37:31 +0300

    create a new js file for a redo

---
+++ trial.js
@@ -0,0 +1,11 @@
+//get elements in global scope.
+let bar = document.querySelector("#character-bar");
+let animalName = document.querySelector("#name");
+let animalImage = document.querySelector("#image");
+let infoP = document.querySelector("#name");
+let infoImg = document.querySelector("#image");
+let submitVotes = document.querySelector("#submit");
+let voteCountSpan = document.querySelector("#vote-count");
+let form = document.querySelector("#votes-form");
+
+let currentCharacterId = null; // Store selected character ID
